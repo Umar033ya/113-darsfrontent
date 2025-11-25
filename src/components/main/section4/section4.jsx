82dc52f4636e98e5b1a407780753469120ba36ee
woxx11 <sheikh07wz@gmail.com>
2025-11-25 15:56:31 +0500

    qoshildi

--- src/components/main/section4/section4.jsx
+++ src/components/main/section4/section4.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import './section4.css'
+import React from "react";
+import "./section4.css";
 function Section4() {
   return (
-    <div>
-        <h1>Section 4</h1>
-    </div>
-  )
+    <>
+      <div className="section-wrapper">
+        <div className="stats">
+          <div className="stat-box">
+            <h1>100%</h1>
+            <p>Organic Products</p>
+          </div>
+
+          <div className="stat-box">
+            <h1>2M+</h1>
+            <p>Yearly Sales</p>
+          </div>
+
+          <div className="stat-box">
+            <h1>1.9M+</h1>
+            <p>Happy Customer</p>
+          </div>
+        </div>
+
+        <div className="main-section">
+          <div className="left">
+            <h1 className="title">
+              The Perfect Beauty <br /> Of You
+            </h1>
+
+            <p className="text">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+
+            <p className="text">
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+
+            <button className="shop-btn">Shop Now</button>
+          </div>
+
+          <div className="right">
+            <img
+              src="/mnt/data/88088bbb-6249-4958-b24c-c58a8804e160.png"
+              alt="Cosmetic"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Section4
+export default Section4;
