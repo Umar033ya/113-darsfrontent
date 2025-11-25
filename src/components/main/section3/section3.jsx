3a25b8a66fe94e237ed65ad6c291ac54582478cf
abduboriyfr <abduboriyking10@gmail.com>
2025-11-25 17:48:18 +0500

    add

--- src/components/main/section3/section3.jsx
+++ src/components/main/section3/section3.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import './section3.css'
+import React from "react";
+import "./section3.css";
+import logo1 from "./Frame (15).png";
+import logo2 from "./pump-bottle-02 1.png";
+import logo3 from "./jar-15 1.png";
+
 function Section3() {
   return (
-    <div>
-        <h1>Section 3</h1>
+    <div className="section3">
+      <div className="section3-parent">
+        <div className="section3-next">
+          <h3>Our Porular Product</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+        </div>
+        <div className="section3-n-text">
+          <p>
+            <a href="#">Discover Our Products</a>
+            <img src={logo1} alt="" />
+          </p>
+        </div>
+      </div>
+     <div className="section3-parent-card">
+     <div className="section3-parent2">
+        <div className="sect3-card">
+          <img src={logo2} alt="rasm" />
+          <h3>Cosmetic products</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+      <div className="sect3-card-button">
+      <div className="sect3-card-b">
+         <button>Buy Now</button>
+         <button>Add To Cart</button>
+         </div>
+       
+      </div>
+        </div>
+      </div>
+
+      <div className="section3-parent2">
+        <div className="sect3-card">
+          <img src={logo3} alt="rasm" />
+          <h3>Cosmetic products</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+          </p>
+      <div className="sect3-card-button">
+      <div className="sect3-card-b">
+         <button>Buy Now</button>
+         <button>Add To Cart</button>
+         </div>
+        
+      </div>
+        </div>
+      </div>
+     </div>
     </div>
-  )
+
+
+
+  );
 }
 
-export default Section3
+export default Section3;
